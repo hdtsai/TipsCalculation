54a468fcf7a3a1c0c4ba9cbf5ab7920a0551a713
lamberttao <cjerma908@gmail.com>
2022-07-10 02:54:13 -0700

    basic layout design

--- app/src/main/java/com/example/tipscalculation/MainActivity.kt
+++ app/src/main/java/com/example/tipscalculation/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.tipscalculation
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.tipscalculation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val pretaxNumber = binding.editText.text
+        Log.d(TAG, "onCreate: $pretaxNumber")
     }
 }
